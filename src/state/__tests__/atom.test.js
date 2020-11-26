9b1f205fc16066bccf2fd388be710505acdd3d96
maxime-conan <maxime@lajavaness.com>
2020-11-26 10:12:50 +0100

    feat: Update atoms tests

--- src/state/__tests__/atom.test.js
+++ src/state/__tests__/atom.test.js
@@ -1,11 +1,43 @@
 import { snapshot_UNSTABLE } from "recoil"
-import { pokemonListUrl } from "../atoms"
+import { pokemonIdAtom, pokemonListUrlAtom } from "../atoms"
 
 describe("atom", () => {
-  it("display the correct default value", () => {
-    const initialSnapshot = snapshot_UNSTABLE()
-    expect(initialSnapshot.getLoadable(pokemonListUrl).valueOrThrow()).toBe(
-      `${process.env.REACT_APP_BASE_URL}?limit=30&offset=0`
-    )
+  describe("pokemonListUrlAtom", () => {
+    it("display the correct default value", () => {
+      const initialSnapshot = snapshot_UNSTABLE()
+      expect(
+        initialSnapshot.getLoadable(pokemonListUrlAtom).valueOrThrow()
+      ).toBe(`${process.env.REACT_APP_BASE_URL}?limit=30&offset=0`)
+    })
+
+    it("display the correct default value", () => {
+      const initialSnapshot = snapshot_UNSTABLE(({ set }) =>
+        set(
+          pokemonListUrlAtom,
+          `${process.env.REACT_APP_BASE_URL}?limit=60&offset=30`
+        )
+      )
+      expect(
+        initialSnapshot.getLoadable(pokemonListUrlAtom).valueOrThrow()
+      ).toBe(`${process.env.REACT_APP_BASE_URL}?limit=60&offset=30`)
+    })
+  })
+
+  describe("pokemonIdAtom", () => {
+    it("display the correct default value", () => {
+      const initialSnapshot = snapshot_UNSTABLE()
+      expect(initialSnapshot.getLoadable(pokemonIdAtom(1)).valueOrThrow()).toBe(
+        1
+      )
+    })
+
+    it("set the correct updated value", () => {
+      const initialSnapshot = snapshot_UNSTABLE(({ set }) =>
+        set(pokemonIdAtom(1), 2)
+      )
+      expect(initialSnapshot.getLoadable(pokemonIdAtom(1)).valueOrThrow()).toBe(
+        2
+      )
+    })
   })
 })
